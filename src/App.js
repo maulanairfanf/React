292803470aaa19271cb1a026ac5bfbad130f84d3
maulanairfanf <maulanairfanf@gmail.com>
2021-03-09 14:25:41 +0700

    edit error import app css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-
-import './App.css';
-import 'assets/scss/style/scss'
+import React from "react";
+import "assets/scss/style.scss";
 
 function App() {
-  return (
-    <div className="App">
-      Hai React
-       
-    </div>
-  );
+  return <div className="App">Hai React</div>;
 }
 
 export default App;
- 

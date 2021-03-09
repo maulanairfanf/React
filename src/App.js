56414416b94536f114ba87c0b0a969123c54c52c
maulanairfanf <maulanairfanf@gmail.com>
2021-03-09 14:13:30 +0700

    test commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import 'assets/scss/style/scss'
+
+function App() {
+  return (
+    <div className="App">
+      Hai React
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/landingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import moduleName from "elements/Button"
+
+export default class landingPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}

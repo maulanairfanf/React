fbfa250df4334a76f45b347adf4175a1733aa0bd
maulanairfanf <maulanairfanf@gmail.com>
2021-03-10 22:33:28 +0700

    add image end most picked 50%

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import landingPage from "json/landingPage.json";
 import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
 export default class LandingPage extends Component {
   render() {
     return (
       <>
         <Header {...this.props}></Header>
         <Hero data={landingPage.hero} />
+        <MostPicked data={landingPage.mostPicked} />
       </>
     );
   }

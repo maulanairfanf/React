f552a7d5be25f3e247e219da1cd56f3415dbaee6
maulanairfanf <maulanairfanf@gmail.com>
2021-03-10 09:37:22 +0700

    add section-hero

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Header from "parts/Header";
-
-export default class landingPage extends Component {
+import landingPage from "json/landingPage.json";
+import Hero from "parts/Hero";
+export default class LandingPage extends Component {
   render() {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero data={landingPage.hero} />
       </>
     );
   }

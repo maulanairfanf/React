46503f8bc1ab274862266da0aa0bb36331d1fd25
maulanairfanf <maulanairfanf@gmail.com>
2021-03-24 10:47:06 +0700

    add input date

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
+import Example from "pages/Example";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route path="/q" component={LandingPage}></Route>
+        <Route path="/" component={Example}></Route>
+
       </Router>
     </div>
   );
